800912162cba596763adb06118e4267690e94e61
vantuandat <vantuandat15062000@gmail.com>
2024-12-05 13:37:45 +0700

    Create 2.5.py

---
+++ Bài 2/2.5.py
@@ -0,0 +1,10 @@
+print("Sinh vien:Van Tuan DAt")
+print("MSSV: 235752021610083")
+###################
+a=int(input("Nhap a: "))
+b=int(input("Nhap b: "))
+i=1;
+for j in range(a,b):
+    print("i:",i,"j:",j)
+    print(i,"/",j)
+    print  (i/j);
