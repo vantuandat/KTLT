35256922165d12ea9e2e76a4dac18b89a43cabe0
vantuandat <vantuandat15062000@gmail.com>
2024-12-05 13:42:54 +0700

    Create 2.8.py

---
+++ Bài 2/2.8.py
@@ -0,0 +1,16 @@
+print("Sinh vien:Van Tuan DAt")
+print("MSSV: 235752021610083")
+###################
+
+
+
+a, b = 1, 2
+toral = 0
+print(a,end=" ")
+while (a <=4000000-1):
+    if a % 2 == 0:
+        toral += a
+    a, b = b, a+b
+    print(a,end=" ")
+print("\n sum of prine numbers tern in fibonacci series: ",total)
+
