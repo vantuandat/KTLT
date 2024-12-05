1ea95bf4072e14cc7c66abd7f39301a5380ba211
vantuandat <vantuandat15062000@gmail.com>
2024-12-05 13:54:28 +0700

    Create 3.5.py

---
+++ Bài3/3.5.py
@@ -0,0 +1,12 @@
+print("Sinh vien:Van Tuan DAt")
+print("MSSV: 235752021610083")
+###################
+
+
+a = "Hello Guy!"
+def say():
+   global a
+   a = " Vinh University "
+   print(a)
+say()
+print(a)
