09eacb8dabd5aec3e94d96f50b57d86177438835
vantuandat <vantuandat15062000@gmail.com>
2024-12-05 13:56:18 +0700

    Create 3.7.py

---
+++ Bài3/3.7.py
@@ -0,0 +1,12 @@
+print("Sinh vien:Van Tuan DAt")
+print("MSSV: 235752021610083")
+###################
+
+
+n=int(input("Nhap mot so n:"))
+def checkValue(n): 
+ if n%2 == 0: 
+   print ("Đây là một số chẵn") 
+ else: 
+   print ("Đây là một số lẻ") 
+checkValue(7)
