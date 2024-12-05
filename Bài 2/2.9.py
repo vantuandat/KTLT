8da9c2b63448478a1eabede3d76c97722c09227d
vantuandat <vantuandat15062000@gmail.com>
2024-12-05 13:43:47 +0700

    Create 2.9.py

---
+++ Bài 2/2.9.py
@@ -0,0 +1,10 @@
+print("Sinh vien:Van Tuan DAt")
+print("MSSV: 235752021610083")
+###################
+
+
+str=input("Enter a String")
+dict = {}
+for i in str:
+    dict[i] = str.count(i)
+print (dict)
