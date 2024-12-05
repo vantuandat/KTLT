826c16487af29de6960a6a1994696cb96133a676
vantuandat <vantuandat15062000@gmail.com>
2024-12-05 13:49:55 +0700

    Create 3.1.py

---
+++ Bài3/3.1.py
@@ -0,0 +1,10 @@
+print("Sinh vien:Van Tuan DAt")
+print("MSSV: 235752021610083")
+###################
+
+def sum(a, b):
+ print("sum = " + str(a + b))
+# tinh tong 2 so 4,5
+sum(4, 5)
+# tinh tong 2 so 3,7
+sum(3, 7)
