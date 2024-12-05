48a521210c413598118947ce8f1bce8bffd88a01
vantuandat <vantuandat15062000@gmail.com>
2024-12-05 13:16:58 +0700

    Create 2.1.py

---
+++ Bài 2/2.1.py
@@ -0,0 +1,11 @@
+print("Sinh vien:Van Tuan DAt")
+print("MSSV: 235752021610083")
+###################
+
+
+n1 = int(input("enter n1 value"))
+n2 = int(input("enter n2 value"))
+if n1>n2:
+   print("n1 is big")
+else:
+   print("n2 is big")
