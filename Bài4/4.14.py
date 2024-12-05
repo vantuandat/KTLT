7688b949fa3b13c233f23094e088afb426f6c2ee
vantuandat <vantuandat15062000@gmail.com>
2024-12-05 14:12:27 +0700

    Create 4.14.py

---
+++ Bài4/4.14.py
@@ -0,0 +1,12 @@
+print("Sinh vien:Van Tuan Dat")
+print("MSSV: 235752021610083")
+###################
+
+# Nhập một chuỗi các phần tử từ bàn phím và chuyển nó thành list
+input_list = input("Nhập các phần tử, cách nhau bởi dấu cách: ").split()
+
+# Sắp xếp list theo thứ tự từ điển (tăng dần)
+input_list.sort()
+
+# In list sau khi sắp xếp
+print("List sau khi sắp xếp theo thứ tự từ điển:", input_list)
