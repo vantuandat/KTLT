b321acdf5dc7291f23d84a73e94da725501ce521
vantuandat <vantuandat15062000@gmail.com>
2024-12-05 13:42:13 +0700

    Create 2.7.py

---
+++ Bài 2/2.7.py
@@ -0,0 +1,11 @@
+print("Sinh vien:Van Tuan DAt")
+print("MSSV: 235752021610083")
+###################
+
+
+n=int(input("Nhap vao mot so:"))
+d=dict()
+for i in range(1,n+1):
+    d[i]=i*i
+
+print (d)
