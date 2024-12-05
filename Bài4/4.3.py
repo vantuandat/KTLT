953524487fae5a765b1122b9909ad985b858d883
vantuandat <vantuandat15062000@gmail.com>
2024-12-05 14:04:22 +0700

    Create 4.3.py

---
+++ Bài4/4.3.py
@@ -0,0 +1,10 @@
+print("Sinh vien:Van Tuan DAt")
+print("MSSV: 235752021610083")
+###################
+
+
+s = input('Nhap chuoi s: '). upper()
+for ch in s:
+    if ch not in ['', '\t']:
+        print(ch)
+        
