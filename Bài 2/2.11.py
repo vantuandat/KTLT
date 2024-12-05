fd0a0bfdf3b94699928615be1c6b54b190651fb5
vantuandat <vantuandat15062000@gmail.com>
2024-12-05 13:45:51 +0700

    Create 2.11.py

---
+++ Bài 2/2.11.py
@@ -0,0 +1,15 @@
+print("Sinh vien:Van Tuan DAt")
+print("MSSV: 235752021610083")
+###################
+
+
+
+
+l=[1,'python',4,7]
+k=['cse',2,'guntur',8]
+m=[]
+m.append(1);
+m.append(k);
+print (m)
+d={1:1,2:k,'combine_list':m}
+print(d)
