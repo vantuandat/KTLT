c43b0271c3ed5d4b42402008c06370b0f4721742
vantuandat <vantuandat15062000@gmail.com>
2024-12-05 14:07:26 +0700

    Create 4.9.py

---
+++ Bài4/4.9.py
@@ -0,0 +1,10 @@
+print("Sinh vien:Van Tuan DAt")
+print("MSSV: 235752021610083")
+###################
+
+
+
+# Nhập một chuỗi các phần tử và chuyển nó thành một list
+input_list = input("Nhập các phần tử, cách nhau bởi dấu cách: ").split()
+print("List đã nhập:", input_list)
+
