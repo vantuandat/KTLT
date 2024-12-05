4f78bb500cfd9c0a9568d33f1145e1e7fa096a5f
vantuandat <vantuandat15062000@gmail.com>
2024-12-05 14:05:15 +0700

    Create 4.5.py

---
+++ Bài4/4.5.py
@@ -0,0 +1,10 @@
+print("Sinh vien:Van Tuan DAt")
+print("MSSV: 235752021610083")
+###################
+
+
+ds = input('danh sach: '). split()
+print("ds: ",ds[: :-1])
+for so in ds:
+    print(so)
+    
