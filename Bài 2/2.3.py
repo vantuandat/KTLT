6c44bf3e6733fd0a4274af553fc97c7c686c8cb4
vantuandat <vantuandat15062000@gmail.com>
2024-12-05 13:36:47 +0700

    Create 2.3.py

---
+++ Bài 2/2.3.py
@@ -0,0 +1,11 @@
+print("Sinh vien:Van Tuan DAt")
+print("MSSV: 235752021610083")
+###################
+
+
+n = int(input("Enter a number--->"))
+if n % 2 == 0:
+    print ("EVEN Number");
+else:
+    print ("ODD Number");
+    
