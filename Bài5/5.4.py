ecd947de024e7fab5790719a4ef11466205dacd6
vantuandat <vantuandat15062000@gmail.com>
2024-12-05 14:25:47 +0700

    Create 5.4.py

---
+++ Bài5/5.4.py
@@ -0,0 +1,16 @@
+print("Sinh vien:Van Tuan DAt")
+print("MSSV: 235752021610083")
+###################
+
+
+
+array = list(range(12, 38))  
+# In mảng ban đầu
+print("Mảng ban đầu:", array)
+
+#  Đảo ngược mảng
+reversed_array = array[::-1]
+
+#  In mảng sau khi đảo ngược
+print("Mảng đã đảo ngược:", reversed_array)
+
