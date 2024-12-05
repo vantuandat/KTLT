e8afcbe291a0dfff4e09177fc9af266bcb99b931
vantuandat <vantuandat15062000@gmail.com>
2024-12-05 15:23:44 +0700

    Create 8.4a.py

---
+++ Bài8/8.4a.py
@@ -0,0 +1,21 @@
+print("Sinh vien:Van Tuan Dat")
+print("MSSV:235752021610083")
+##################
+
+
+import tkinter as tk
+
+# Tạo cửa sổ chính
+window = tk.Tk()
+
+# Đặt tiêu đề cho cửa sổ
+window.title("Cửa sổ đồ họa Tkinter")
+
+# Đặt kích thước cho cửa sổ (width x height)
+window.geometry("400x300")
+
+# Đặt một số thuộc tính cho cửa sổ (tuỳ chọn)
+window.configure(bg="lightblue")  # Thay đổi màu nền của cửa sổ
+
+# Hiển thị cửa sổ
+window.mainloop()
